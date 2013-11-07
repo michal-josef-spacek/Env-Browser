fc180dad7880f2b2056b7c775acc3b83a7d4b3aa
Michal Špaček <tupinek@gmail.com>
2013-11-07 11:23:56 +0100

    Prepare for 0.05 version.

--- Browser.pm
+++ Browser.pm
@@ -13,7 +13,7 @@ Readonly::Array our @EXPORT_OK => qw(run);
 Readonly::Scalar our $SPACE => q{ };
 
 # Version.
-our $VERSION = 0.04;
+our $VERSION = 0.05;
 
 # Run browser.
 sub run {
@@ -152,6 +152,6 @@ BSD license.
 
 =head1 VERSION
 
-0.04
+0.05
 
 =cut
